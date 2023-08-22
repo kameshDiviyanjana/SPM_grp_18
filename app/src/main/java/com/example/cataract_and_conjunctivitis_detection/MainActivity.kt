cfb692582a02713b3eda53600e56a487a9cd66a5
Dilshan09 <pereradilshan594@gmail.com>
2023-08-22 13:16:39 +0530

    Initial commit Praneeth

--- app/src/main/java/com/example/cataract_and_conjunctivitis_detection/MainActivity.kt
+++ app/src/main/java/com/example/cataract_and_conjunctivitis_detection/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.cataract_and_conjunctivitis_detection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() { //MainActivity
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
