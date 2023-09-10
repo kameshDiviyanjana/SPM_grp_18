245b100fa81d0c51b77b7f0a8894fba8f14d5791
it21155352 <it21155352@my.sliit.lk>
2023-09-10 13:46:24 +0530

    completed cataract detection v1

--- app/src/main/java/com/example/cataract_and_conjunctivitis_detection/home.kt
+++ app/src/main/java/com/example/cataract_and_conjunctivitis_detection/home.kt
@@ -1,15 +1,33 @@
 package com.example.cataract_and_conjunctivitis_detection
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class home : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        val go = Intent(this,cataract_detection::class.java)
-        startActivity(go)
+         var chenel = findViewById<Button>(R.id.btnchenl)
+          var chatbot = findViewById<Button>(R.id.btnchat)
+        var ctractdet = findViewById<Button>(R.id.btncatr)
+        var reminder = findViewById<Button>(R.id.btnreminder)
+
+
+        ctractdet.setOnClickListener {
+            val go = Intent(this,cataract_detection::class.java)
+            startActivity(go)
+        }
+
+        chatbot.setOnClickListener {
+
+            val go = Intent(this,ChatBot_MainActivity::class.java)
+            startActivity(go)
+        }
+
     }
 }
